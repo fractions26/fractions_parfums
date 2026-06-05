3095795bd26a61d51d754dbc0ec39e7df2c1262b
Douglas Roberto <www.dls14@gmail.com>
2026-06-05 12:44:46 -0300

    fix static storage imagens

--- fractions_parfums/settings.py
+++ fractions_parfums/settings.py
@@ -81,7 +81,8 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 
 MEDIA_URL = '/media/'
