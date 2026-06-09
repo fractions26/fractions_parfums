5cd7bf82eb6d553a978881fff6d64e7b2aac8c37
Douglas Roberto <www.dls14@gmail.com>
2026-06-09 11:07:37 -0300

    Configura SMTP Hostinger

--- fractions_parfums/settings.py
+++ fractions_parfums/settings.py
@@ -214,15 +214,26 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config(
+    'EMAIL_HOST',
+    default='smtp.hostinger.com'
+)
 
-EMAIL_PORT = 587
+EMAIL_PORT = config(
+    'EMAIL_PORT',
+    default=587,
+    cast=int
+)
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config(
+    'EMAIL_USE_TLS',
+    default=True,
+    cast=bool
+)
 
 EMAIL_HOST_USER = config(
     'EMAIL_HOST_USER',
-    default=''
+    default='contato@fractions.com.br'
 )
 
 EMAIL_HOST_PASSWORD = config(
