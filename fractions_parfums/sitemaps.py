86de48a3bbc84494e8fb782d186a47278f6d37d9
Douglas Roberto <www.dls14@gmail.com>
2026-06-16 07:13:22 -0300

    google sitemaps

--- fractions_parfums/sitemaps.py
+++ fractions_parfums/sitemaps.py
@@ -1,7 +1,13 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from apps.produtos.models import Produto
 
+
+# ✅ PÁGINAS ESTÁTICAS
 class StaticSitemap(Sitemap):
+
+    priority = 0.8
+    changefreq = 'weekly'
 
     def items(self):
         return [
@@ -28,3 +34,16 @@ class StaticSitemap(Sitemap):
 
         if item == 'quem_somos':
             return '/quem-somos/'
+
+
+# ✅ PRODUTOS (ESSENCIAL)
+class ProdutoSitemap(Sitemap):
+
+    changefreq = "daily"
+    priority = 0.9
+
+    def items(self):
+        return Produto.objects.all()  # ✅ pega todos
+
+    def location(self, obj):
+        return f"/produtos/detalhe/{obj.slug}/"
